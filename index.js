7e12a6a81811c1e74a4371da051a90394570e367
SoyJBL <yosoyjbl@gmail.com>
2024-09-21 16:13:31 -0600

    test

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import modulos from './routers/modulos.js';
-import cookieParser from 'cookie-parser';
-import login from './routers/login.js';
-import cors from 'cors';
-import generateContent from './gemini-integration/gemini.js';
-import connectDB from './DataBase/db.js';
+import express from "express";
+import bodyParser from "body-parser";
+import modulos from "./routers/modulos.js";
+import cookieParser from "cookie-parser";
+import login from "./routers/login.js";
+import cors from "cors";
+import generateContent from "./gemini-integration/gemini.js";
+import connectDB from "./DataBase/db.js";
 const app = express();
 
 
@@ -15,15 +15,15 @@ app.use(express.json());
 
 
 app.use(bodyParser.json());
-app.use(cors());
-app.use('/api/modulos', modulos);
-app.use('/login', login);
+app.use();
+app.use("/api/modulos", modulos);
+app.use("/login", login);
 app.get("/gemini", generateContent);
 
 connectDB();
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
 
 
